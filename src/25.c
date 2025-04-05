4e00e55df7bf88de938b8a587f2b356baebfc1d1
abebfortna146 <abebfortna146@gmail.com>
2025-04-05 05:48:12 +0000

    Add 25.c

---
+++ src/25.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1 = 5;
+    int num2 = 3;
+
+    if (num1 > num2) {
+        printf("The first number is greater than the second number.\n");
+    } else {
+        printf("The first number is not greater than the second number.\n");
+    }
+
+    return 0;
+}
